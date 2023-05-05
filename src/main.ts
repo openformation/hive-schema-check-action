3d453df75cb5b6a8cd77f8b13807496128a5bd9d
Nils Jansen <nils.jansen@openformation.io>
2023-05-05 12:36:30 +0200

    feat: added actual check

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,95 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { existsSync } from 'fs'
+import { execa } from 'execa'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // Inputs
+    const serviceName = core.getInput('service-name', { required: true })
+    if (!serviceName || serviceName.length === 0) {
+      throw new Error('Input "service-name" is required')
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const schemaPath = core.getInput('schema-path', { required: true })
+    if (!schemaPath || schemaPath.length === 0) {
+      throw new Error('Input "schema-path" is required')
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    const registryToken = core.getInput('hive-registry-access-token', {
+      required: true
+    })
+    if (!registryToken || registryToken.length === 0) {
+      throw new Error('Input "hive-registry-access-token" is required')
+    }
+
+    const shouldCommentPR = core.getBooleanInput('comment-pr')
+
+    const githubToken = core.getInput('github-token', { required: false })
+    if (shouldCommentPR && (!githubToken || githubToken.length === 0)) {
+      throw new Error(
+        'Input "github-token" is required when PR comments are enabled'
+      )
+    }
+
+    const registryEndpoint = 'https://registry.hive.openformation.io/graphql'
+
+    // Check if schema file exists
+    if (!existsSync(schemaPath)) {
+      throw new Error('The schema file could not be found at the provided path')
+    }
+
+    // Install Hive CLI
+    const curl = execa('curl', ['-sSL', 'https://graphql-hive.com/install.sh'])
+    if (!curl.pipeStdout) {
+      throw new Error('Hive CLI could not be installed.')
+    }
+    await curl.pipeStdout(execa('sh'))
+
+    core.debug('Hive CLI installed')
+
+    // Run schema check
+    const { stdout: result, exitCode } = await execa(
+      'hive',
+      [
+        'schema:check',
+        schemaPath,
+        '--service',
+        serviceName,
+        '--registry.endpoint',
+        registryEndpoint
+      ],
+      {
+        env: {
+          HIVE_TOKEN: registryToken
+        },
+        reject: false
+      }
+    )
+
+    const schemaCheckPassed = exitCode === 0
+
+    core.debug(result)
+
+    const pullNumber = github.context.payload.pull_request?.number
+    if (pullNumber && shouldCommentPR) {
+      const message = `Hive schema check result:\n${result}`
+
+      const octokit = github.getOctokit(githubToken)
+      try {
+        await octokit.rest.issues.createComment({
+          ...github.context.repo,
+          issue_number: pullNumber,
+          body: message
+        })
+      } catch (error) {
+        throw new Error('Failed to comment on PR')
+      }
+    }
+
+    if (!schemaCheckPassed) {
+      core.setFailed(`Schema check failed:\n${result}`)
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
